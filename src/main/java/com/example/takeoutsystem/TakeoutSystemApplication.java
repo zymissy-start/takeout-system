1c8472e1d79483e29171811c7d149632b9f7d3b7
zymissy-start <1724284903@qq.com>
2026-06-18 14:10:57 +0800

    [项目初始化]创建SpringBoot基础工程

---
+++ src/main/java/com/example/takeoutsystem/TakeoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.takeoutsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeoutSystemApplication.class, args);
+	}
+
+}
