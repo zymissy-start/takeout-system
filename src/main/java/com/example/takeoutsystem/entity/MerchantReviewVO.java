be01714a7c2b8f69a9d9d94e3aa7d622881408b3
1112xtreme <3405621317@qq.com>
2026-06-28 01:53:14 +0800

    商家可以通过订单详情联系用户和骑士，调整了店铺信息界面

---
+++ src/main/java/com/example/takeoutsystem/entity/MerchantReviewVO.java
@@ -0,0 +1,62 @@
+package com.example.takeoutsystem.entity;
+
+/**
+ * 商家收到的用户评价展示对象。
+ */
+public class MerchantReviewVO {
+
+    private Integer reviewId;
+    private Integer orderId;
+    private String userName;
+    private Integer score;
+    private String content;
+    private String createTime;
+
+    public Integer getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Integer reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
